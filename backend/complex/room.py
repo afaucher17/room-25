c42b172409b70542fa93d181574d814a2592d2f6
Aymeric FAUCHER <afaucher@e2r12p12.42.fr>
2016-09-05 16:20:17 +0200

    room.py

---
+++ backend/complex/room.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+from enum import IntEnum
+class RoomType(IntEnum):
+    central_room = 1
+    room_25 = 2
+    vision_chamber = 3
+    moving_chamber = 4
+    control_chamber = 5
+    twin_chamber = 6
+    empty_chamber = 7
+    vortex_room = 8
+    prison_cell = 9
+    cold_chamber = 10
+    dark_chamber = 11
+    trapped_chamber = 12
+    mortal_chamber = 13
+    illusion_chamber = 14
+    flooded_chamber = 15
+    acid_bath = 16
+
+class RoomDanger(IntEnum):
+    green = 1
+    yellow = 2
+    red = 3
+    neutral = 4
+
+class Room:
+    'A room in the complex, they have different attributes'
+
+    def __init__(self, room_type):
+        self.room_type = room_type
+
+    def get_room_ranger(self):
+        if self.room_type < 3:
+            return RoomDanger.neutral
+        elif self.room_type < 8:
+            return RoomDanger.green
+        elif self.room_type < 12:
+            return RoomDanger.yellow
+        else:
+            return RoomDanger.red
+
+    def displayRoomType(self):
+        print(self.room_type)
+
